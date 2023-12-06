8ff29c9ea330cb4937444d98eee80f760f01c83c
MrBregott <keybind@tuta.io>
2023-12-06 17:36:33 +0100

    Updated getResultsSearch to dynamically add search parameter to the url

--- test-project/Yoda Gaming/src/websiteSource.js
+++ test-project/Yoda Gaming/src/websiteSource.js
@@ -3,8 +3,28 @@ import { API_KEY, BASE_URL } from "./apiConfig";
 function getResultsSearch(searchParams)
 {
 
-    const URL = BASE_URL + "games?key=" + API_KEY + "&search=" + searchParams + "&search_precise=true";
+    let URL = BASE_URL + "games?key=" + API_KEY + "&search_precise=true%search_exact=true";
 
+    if (searchParams.search){ 
+        URL += "&search=" + searchParams.search;
+    }
+    if (searchParams.genres){
+        URL += "&genres=" + searchParams.genres;
+    }
+    if (searchParams.tags){
+        URL += "&tags=" + searchParams.tags;
+    }
+    if (searchParams.metacritic_min){
+        if (searchParams.metacritic_max){
+            URL += "&metacritic=" + searchParams.metacritic_min + "," + searchParams.metacritic_max;
+        }
+        else{
+            URL += "&metacritic=" + searchParams.metacritic_min + ",100";
+        }
+    }
+    if (searchParams.ordering){
+        URL += "&ordering=" + searchParams.ordering; // Add "-" before the order variable to reverse the order
+    }
 
     function getTheJSON_ACB(respons){
         if(!respons.ok){
@@ -21,5 +41,9 @@ function getResultsSearch(searchParams)
     return fetch(URL).then(getTheJSON_ACB).then(giveOnlyRelevantInfoACB)
 }
 
+function getGameDetails(gameID){
 
-export { getResultsSearch};
+}
+
+
+export { getResultsSearch, getGameDetails};
