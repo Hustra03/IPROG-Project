74f956d1205732ebe76a759323b6228c527e8431
MrBregott <keybind@tuta.io>
2023-12-06 15:33:23 +0100

    Updated websiteSource.js to make API call with string as parameter

--- test-project/Yoda Gaming/src/websiteSource.js
+++ test-project/Yoda Gaming/src/websiteSource.js
@@ -1,9 +1,24 @@
 import { API_KEY, BASE_URL } from "./apiConfig";
 
-function getResultsSearch()
+function getResultsSearch(searchParams)
 {
 
-    return null;
+    const URL = BASE_URL + "games?key=" + API_KEY + "&search=" + searchParams + "&search_precise=true";
+
+
+    function getTheJSON_ACB(respons){
+        if(!respons.ok){
+            throw new Error("Something went wrong.");
+        }
+        return respons.json();
+    }
+
+    function giveOnlyRelevantInfoACB(json){
+        //console.log(json); //Uncomment to see the the api response object
+        return json.results;
+
+    }
+    return fetch(URL).then(getTheJSON_ACB).then(giveOnlyRelevantInfoACB)
 }
 
 
