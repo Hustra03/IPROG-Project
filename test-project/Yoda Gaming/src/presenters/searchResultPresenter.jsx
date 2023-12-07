7f4f207a0d5ee813b6fb4afe7e312f7df7c0f238
EliazBid <124061492+EliazBid@users.noreply.github.com>
2023-12-07 15:48:26 +0100

    Update searchResultPresenter.jsx

--- test-project/Yoda Gaming/src/presenters/searchResultPresenter.jsx
+++ test-project/Yoda Gaming/src/presenters/searchResultPresenter.jsx
@@ -1,7 +1,27 @@
+import {SearchResultView} from "../views/searchResultView";
+
 export default
-    function SearchResultPresenter() {
+    function SearchResultPresenter(props) {
     return (
         <div>
+            {showResult()}
         </div>
+       
     );
+
+    function showResult(){
+        if (!props.model.searchResultsPromiseState.promise) {
+            return <div>no data found</div>
+        }
+        if (props.model.searchResultsPromiseState.data) {
+            console.log(props.model.searchResultsPromiseState.data)
+            return <SearchResultView model={props.model} searchResults={props.model.searchResultsPromiseState.data}/>
+        }
+
+        if (!props.model.searchResultsPromiseState.error){
+            return <div>loading</div>
+        }
+        console.log(props.model.searchResultsPromiseState.error)
+        return <div>{props.model.searchResultsPromiseState.error}</div>
+    }
 }
