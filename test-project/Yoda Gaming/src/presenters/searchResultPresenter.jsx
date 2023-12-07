fee12f6ea6627453bba3881cb8acb379d3665b6e
EliazBid <eliaz@kth.se>
2023-12-07 15:37:08 +0100

    added very basic searchResult implementation

--- test-project/Yoda Gaming/src/presenters/searchResultPresenter.jsx
+++ test-project/Yoda Gaming/src/presenters/searchResultPresenter.jsx
@@ -1,7 +1,31 @@
+import {SearchResultView} from "../views/searchResultView";
+
 export default
-    function SearchResultPresenter() {
+    function SearchResultPresenter(props) {
     return (
         <div>
+            {showResult()}
+            {/* <SearchResultView
+                model={props.model}
+                 searchResults={props.model.searchResultsPromiseState}
+            />    */}
         </div>
+       
     );
+
+    function showResult(){
+        if (!props.model.searchResultsPromiseState.promise) {
+            return <div>no data found</div>
+        }
+        if (props.model.searchResultsPromiseState.data) {
+            console.log(props.model.searchResultsPromiseState.data)
+            return <SearchResultView model={props.model} searchResults={props.model.searchResultsPromiseState.data}/>
+        }
+
+        if (!props.model.searchResultsPromiseState.error){
+            return <div>loading</div>
+        }
+        console.log(props.model.searchResultsPromiseState.error)
+        return <div>{props.model.searchResultsPromiseState.error}</div>
+    }
 }
