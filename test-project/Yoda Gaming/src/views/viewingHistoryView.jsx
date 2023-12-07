5a5d57eb4e1f80fa924204239dfa07e855b73708
eripau <erik.paulinder@gmail.com>
2023-12-07 11:22:13 +0100

    modified:   test-project/Yoda Gaming/src/views/viewingHistoryView.jsx

--- test-project/Yoda Gaming/src/views/viewingHistoryView.jsx
+++ test-project/Yoda Gaming/src/views/viewingHistoryView.jsx
@@ -3,7 +3,7 @@ function ViewingHistoryView(props) {
     return (
         <div className="ViewingHistory">
 
-            <h1>View Hisotry</h1>
+            <h1>View History</h1>
             <p>testesttest</p>
             {console.log("test2")}
         </div>
