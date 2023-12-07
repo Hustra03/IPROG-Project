4c57a21d88df6ad449e5cc3a5ed1934517d468b5
EliazBid <eliaz@kth.se>
2023-12-07 17:22:18 +0100

    small css changes for searchResult

--- test-project/Yoda Gaming/src/views/searchResultView.jsx
+++ test-project/Yoda Gaming/src/views/searchResultView.jsx
@@ -6,8 +6,10 @@ function SearchResultView(props) {
     function backToSavedPagesCB() {window.location.hash = "#/"; }
     return(
     <div className="searchResults">
+        <span>{props.searchResult}</span>
+        <CButton onClick={backToSavedPagesCB} className="searchResultInitial">back to initial</CButton>
         {props.searchResults.map(searchResultsMapCB)}
-        <CButton onClick={backToSavedPagesCB} >back to initial</CButton>
+        <div>hi</div>
     </div>
     )
 
@@ -18,9 +20,12 @@ function SearchResultView(props) {
         }
 
         return (
-            <div onClick={onSearchResultClickCB} className="searchResult" style="background-color:">
-                <span className="searchResultInfo">{game.name}</span>
-                <img classname="searchResultImage" src={game.background_image} width="150"></img>
+            <div onClick={onSearchResultClickCB} className="searchResult">
+                <div class="titleAndInfo">
+                    <div className="searchResultTitle">{game.name}</div>
+                    <span className="searchResultInfo">Here goes some game info about each game Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt pharetra nibh ut egestas. Sed eros enim, laoreet vitae enim eget, facilisis dapibus ipsum. Morbi quam libero, ultrices quis efficitur in, imperdiet in diam. Morbi auctor ante quis aliquet interdum. Praesent ex massa, tristique mollis mauris feugiat, vulputate sollicitudin enim. Ut et efficitur arcu. Cras tincidunt ut metus quis iaculis. Duis lacus nisi, feugiat sed tellus a, vehicula blandit lectus. Nunc blandit ac eros at molestie. Nam bibendum aliquam arcu et malesuada. Proin suscipit elementum egestas. Pellentesque eget malesuada nisl.</span>
+                </div>
+                <img className="searchResultImage" src={game.background_image} width="150"></img>
             </div>
         )
     }
